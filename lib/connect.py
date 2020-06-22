3ccead4409ac893f5e74e48f4f4a79f577e4087b
secondaryfun <c.birkenhagen@gmail.com>
2020-06-22 07:16:12 -0400

    connection modified for deployment

--- lib/connect.py
+++ lib/connect.py
@@ -1,8 +1,15 @@
 from peewee import *
+from dotenv import load_dotenv
+import os
 
+project_folder = os.path.expanduser('../')
+load_dotenv(os.path.join(project_folder, '.env'))
 
+DB_PASS = os.getenv('DB_PASS')
+DB_USER = os.getenv('DB_USER')
+DB_HOST = os.getenv('DB_HOST')
 
-db = PostgresqlDatabase('spm', user="postgres",
-                        password='password', host='localhost', port=5432)
+db = PostgresqlDatabase('spm', user=DB_USER,
+                        password=DB_PASS, host='localhost', port=5432)
 
 db.connect()
--- lib/seedcards.py
+++ lib/seedcards.py
@@ -1,10 +1,10 @@
 from peewee import *
-import json, csv, sys
+import json
+import csv
+import sys
 from psycopg2 import connect, Error
 from models import *
 from connect import db
-
-
 
 
 tables = [ExploreCard]
@@ -12,5 +12,34 @@ tables = [ExploreCard]
 db.drop_tables(tables)
 db.create_tables(tables)
 
-# ExploreCard.select()
-#
+with open('./explorecards.json') as json_data:
+    card_list = json.load(json_data)
+
+
+for card in card_list:
+    for value in card.values():
+        if value == "-":
+            value = ""
+
+    ExploreCard(
+        name=card["NAME"],
+        sort=card["Quick Sort"],
+        char_type=card["TYPE"],
+        affinity=card["AFFINITY"],
+        move=card["MOVE"],
+        ap=card["AP"],
+        strength=card["STR"],
+        arm=card["ARM"],
+        will=card["WILL"],
+        dex=card["DEX"],
+        heart=card["HEART"],
+        sk_pn=card["SK/PN"],
+        bit=card["BIT"],
+        abilities=card["ABILITIES"],
+        actions=card["ACTIONS"],
+        card_back=card["CARD BACK"],
+        flavor=card["FLAVOR"],
+        product=card["PRODUCT"],
+        realm=card["REALM"],
+        status=card["STATUS"]
+    ).save()
