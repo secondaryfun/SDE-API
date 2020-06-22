3817c80ecd74424295f65b42ba4204436fcb96e4
secondaryfun <c.birkenhagen@gmail.com>
2020-06-22 17:44:23 -0400

    prepping for deploy

--- lib/connect.py
+++ lib/connect.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 import os
 from flask import Flask
 
-project_folder = os.path.expanduser('~/coding/sei/projects/SDE-API/lib/')
+project_folder = os.path.expanduser('./')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 DB_PASS = os.getenv('DB_PASS')
 DB_USER = os.getenv('DB_USER')
 DB_HOST = os.getenv('DB_HOST')
+FLASK
 
 db = PostgresqlDatabase('d547nnloq163dj', user=DB_USER,
                         password=DB_PASS, host=DB_HOST, port=5432)
